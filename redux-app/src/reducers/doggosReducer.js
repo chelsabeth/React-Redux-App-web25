a7068dbaf0d44ee8825ee63e389d1ea89f04119e
Chelsea Wetzel <chelseagilmore14@gmail.com>
2019-12-12 16:25:51 -0500

    doggos reducer set up

--- redux-app/src/reducers/doggosReducer.js
+++ redux-app/src/reducers/doggosReducer.js
@@ -1,3 +1,9 @@
+import {
+    FETCH_DOGGOS_START,
+    FETCH_DOGGOS_SUCCESS,
+    FETCH_DOGGOS_FAIL,
+} from "../actions";
+
 const initialState = {
     doggos: "",
     isFetching: false,
@@ -5,7 +11,28 @@ const initialState = {
 }
 
 const reducer = (state = initialState, action) => {
-
+    switch(action.type) {
+        case FETCH_DOGGOS_START:
+            return {
+                ...state,
+                isFetching: true
+            };
+            case FETCH_DOGGOS_SUCCESS:
+                return {
+                    ...state,
+                    doggos: action.payload,
+                    isFetching: false,
+                    error: ""
+                };
+                case FETCH_DOGGOS_FAIL:
+                    return {
+                        ...state,
+                        isFetching: false,
+                        error: action.payload
+                    };
+                    default: 
+                    return state;
+    }
 }
 
 export default reducer;
