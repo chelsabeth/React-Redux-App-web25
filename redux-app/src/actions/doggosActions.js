9643a1d3860906271e61ab21a2aac93e18f16d29
Chelsea Wetzel <chelseagilmore14@gmail.com>
2019-12-12 18:07:52 -0500

    getting response from API, need to map through array

--- redux-app/src/actions/doggosActions.js
+++ redux-app/src/actions/doggosActions.js
@@ -9,7 +9,8 @@ export const getDoggos = () => dispatch => {
     axios
         .get("https://dog.ceo/api/breed/pitbull/images")
         .then(res => {
-            dispatch({ type: FETCH_DOGGOS_SUCCESS, payload: res.data.value });
+            console.log("Data from API", res.data);
+            dispatch({ type: FETCH_DOGGOS_SUCCESS, payload: res.data });
         })
         .catch(err => {
             dispatch({ type: FETCH_DOGGOS_FAIL, payload: err.response })
